4634212ef060b185cdda4d25e05118a87ca8f009
Chomper64 <richard.pump@gmx.de>
2013-05-16 15:47:22 +0200

    Incoming enemy Ships

--- project_helix.cpp
+++ project_helix.cpp
@@ -1,3 +1,14 @@
+#include <osgDB/ReadFile>
+#include <osgViewer/Viewer>
+#include <osg/Fog>
+
+using namespace osg;
 int main(void) {
-  return 0;
+    ref_ptr<Group> root = new Group();
+    ref_ptr<Node> ship = osgDB::readNodeFile( "../resources/cruiser.obj" );
+    
+    root->addChild(ship);
+    osgViewer::Viewer viewer;
+    viewer.setSceneData( root.get() );
+    return viewer.run();
 }
