807247fba9b8c2fa96d9d3534cdc54729bfc8dd6
Benjamin <benjamin.held@stud.fh-hannover.de>
2013-05-20 10:50:20 +0200

    fixing math fails in torus

--- objects/torus.cpp
+++ objects/torus.cpp
@@ -4,11 +4,11 @@
 
 using namespace osg;
 
-ph::Torus::Torus(const double iRadius, const double oRadius, const int phiIteration, const int thetaIteration) {
-	this->iRadius = iRadius;
-	this->oRadius = oRadius;
-	this->phiIteration = phiIteration;
-	this->thetaIteration = thetaIteration;
+ph::Torus::Torus(const double iRadius, const double tRadius, const int phiIteration, const int thetaIteration) {
+	this->iRadius = iRadius; //radius from origin to the torus
+	this->tRadius = tRadius; //radius of the torus
+	this->phiIteration = phiIteration; //iterations for the torus-circle
+	this->thetaIteration = thetaIteration; //iterations of the planar circle
 	this->torus = new Geometry;
 	this->addDrawable(this->torus.get());
 	this->compute();
@@ -28,20 +28,20 @@ void ph::Torus::setCoordinates() {
     ref_ptr<Vec3Array> normals = new Vec3Array();
 	ref_ptr<Vec2Array> texcoords = new Vec2Array;
 	
-	double phiStep = (oRadius - iRadius) / phiIteration;
-	double thetaStep = (oRadius - iRadius) / thetaIteration;
+	double phiStep = this->iRaidus / phiIteration;
+	double thetaStep = this->tRadius / thetaIteration;
 	Vec3d coords;
 	
-	for (double i = 0, phi = 0; i <= this->phiIteration; i++, phi += phiStep) {
-        for (double j = 0, theta = 0; j <= this->thetaIteration; j++, theta += thetaStep) {
+	for (double i = 0, theta = 0; i <= this->phiIteration; i++, theta += thetaStep) {
+        for (double j = 0, phi = 0; j <= this->thetaIteration; j++, phi += phiStep) {
             coords = Vec3d(
-                this->(iRadius + oRadius* cos(2*PI*phi))* cos(2*PI*theta), // x
-                this->(iRadius + oRadius* cos(2*PI*phi))* sin(2*PI*theta), // y
-                this->0.5* sin(2*PI*phi)              // z
-                /*normally: oRadius* sin(2*PI*phi), but we need the other notation for 
+                (this->iRadius + this->tRadius* cos(2*PI*phi))* cos(2*PI*theta), // x
+                (this->iRadius + this->tRadius* cos(2*PI*phi))* sin(2*PI*theta), // y
+                0.5* sin(2*PI*phi)              // z
+                /*normally: this->tRadius* sin(2*PI*phi), but we need the other notation for 
                 			our rings*/
             );
-            texcoords->push_back(Vec2(phi, theta));
+            texcoords->push_back(Vec2(theta, phi));
             vertices->push_back(coords);
             coords.normalize();
             normals->push_back(coords);
