b3a6f41f1fbdf76d483f9b87df45841535411d96
Benjamin <benjamin.held@stud.fh-hannover.de>
2013-05-23 14:22:03 +0200

    Torus with correct triangles and indices

--- runners/torus.cpp
+++ runners/torus.cpp
@@ -1,6 +1,8 @@
 #include <osgViewer/Viewer>
+#include <osg/Texture2D>
 #include <osg/MatrixTransform>
 #include <osg/PolygonMode>
+#include <osgDB/ReadFile>
 
 #include "../3rdparty/osgToy/Normals.h"
 using namespace osgToy;
@@ -8,26 +10,33 @@ using namespace osgToy;
 #include "../objects/torus.h"
 
 int main(void) {
-    ref_ptr<ph::Torus> torus = new ph::Torus(6, 1.5, 75, 75);
+    ref_ptr<ph::Torus> torus = new ph::Torus(6, 1.5, 100, 100);
     ref_ptr<Group> root = new Group();
     root->addChild(torus.get());
 
     // Default ist NORMAL
     torus->setStyle(ph::Torus::FLAT);
+    
+    ref_ptr<Texture2D> texture = new Texture2D;
+    ref_ptr<Image> image = osgDB::readImageFile("../Textures/EarthMap.jpg");
+    texture->setWrap(Texture::WRAP_S, Texture::CLAMP);
+    texture->setImage(image.get());
+    
+    root->getOrCreateStateSet()->setTextureAttributeAndModes(0, texture.get());
 
     // Alternativ
     //ref_ptr<ph::Torus> torus = new ph::FlatTorus(6, 1.5, 75, 75);
 
     // Zum Normalen anschauen
-    // ref_ptr<VertexNormals> vNormals = new VertexNormals(torus.get());
-    // ref_ptr<SurfaceNormals> sNormals = new SurfaceNormals(torus.get());
-    // root->addChild(vNormals.get());
-    // root->addChild(sNormals.get());
+    ref_ptr<VertexNormals> vNormals = new VertexNormals(torus.get());
+    ref_ptr<SurfaceNormals> sNormals = new SurfaceNormals(torus.get());
+    //root->addChild(vNormals.get());
+    //root->addChild(sNormals.get());
 
     // Wenn man die Dreiecke mal sehen will:
-    // ref_ptr<PolygonMode> pm = new PolygonMode;
-    // pm->setMode(PolygonMode::FRONT_AND_BACK, PolygonMode::LINE);
-    // root->getOrCreateStateSet()->setAttribute(pm.get());
+    ref_ptr<PolygonMode> pm = new PolygonMode;
+    pm->setMode(PolygonMode::FRONT_AND_BACK, PolygonMode::LINE);
+    //root->getOrCreateStateSet()->setAttribute(pm.get());
 
     osgViewer::Viewer viewer;
     viewer.setSceneData(root.get());
