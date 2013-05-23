a3d524135e3b776f9034483f77c11ca88a8bc731
Benjamin <benjamin.held@stud.fh-hannover.de>
2013-05-23 08:16:05 +0200

    correct sphere

--- objects/sphere.cpp
+++ objects/sphere.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <iostream>
 
 #include "sphere.h"
 
@@ -38,6 +39,7 @@ void ph::Sphere::setCoordinates() {
                 this->radius*sin(2*PI*s)*cos(PI*t-PI_2), // y
                 this->radius*sin(PI*t-PI_2)              // z
             );
+             std::cout << this->radius*cos(2*PI*s)*cos(PI*t-PI_2) << ", " << this->radius*sin(2*PI*s)*cos(PI*t-PI_2) << ", " << this->radius*sin(PI*t-PI_2) << std::endl;
             texcoords->push_back(Vec2(s, t));
             vertices->push_back(coords);
             coords.normalize();
@@ -56,11 +58,9 @@ void ph::Sphere::setIndicies() {
 
     for (int i = 0; i < this->lsteps; i++) {
         for (int j = 0; j <= this->wsteps; j++) {
-            indices->push_back(i*(this->wsteps+1)+j);
-            indices->push_back((i+1)*(this->wsteps+1)+j);
+            indices->push_back((i * lsteps) + (j % lsteps));
+            indices->push_back(((i + 1) * lsteps) + (j % lsteps));
         }
-        indices->push_back((i+1)*(this->wsteps+1)+this->wsteps);
-        indices->push_back((i+1)*(this->wsteps+1));
     }
     
     this->sphere->addPrimitiveSet(indices.get());
@@ -79,23 +79,32 @@ StackedSphere::StackedSphere(const double radius, const int wsteps, const int ls
     for (int i = 0; i <= wsteps; i++) {
         for (int j = 0; j < lsteps; j++) {
             theta = i * PI / wsteps;
-            phi = j * 2 * PI / lsteps;
+            phi = j * 2 * PI / (lsteps-1);
             coords = Vec3d(
                 radius * cos(phi) * sin(theta), 
                 radius * sin(phi) * sin(theta), 
                 radius * cos(theta)
             );
-            texcoords->push_back(Vec2d(i/(double) wsteps, j/(double) lsteps)); // noch blödsinn
+            //std::cout << radius * cos(phi) * sin(theta) << ", " << radius * cos(phi) * sin(theta) << ", " << radius * cos(theta) << std::endl;
             verticies->push_back(coords);
             coords.normalize();
             normals->push_back(coords);
         }
     }
     
+    for (int j = lsteps; j >= 0; j--) {
+        for (int i = 0; i < wsteps; i++) {
+        	texcoords->push_back(Vec2d(i/(double) wsteps, j/(double) lsteps));
+            //std::cout << i/(double) wsteps << ", " << j/(double) lsteps << std::endl;
+        }
+    }
+    
     for (int i = 0; i < wsteps; i++) {
         for (int j = 0; j <= lsteps; j++) {
-            indicies->push_back((i * lsteps) + (j % lsteps));
+     
+            indicies->push_back((i * lsteps + j % lsteps));
             indicies->push_back(((i + 1) * lsteps) + (j % lsteps));
+            //std::cout << (i * lsteps) + (j % lsteps) << ", " << ((i + 1) * lsteps) + (j % lsteps) << std::endl;
         }
     }
 
--- runners/sphere.cpp
+++ runners/sphere.cpp
@@ -8,19 +8,20 @@
 
 
 int main(void) {
-    // ref_ptr<ph::Sphere> sphere = new ph::Sphere(5, 100, 100);
-    ref_ptr<StackedSphere> sphere = new StackedSphere(5, 100, 100);
+    //ref_ptr<ph::Sphere> sphere = new ph::Sphere(5, 10, 10);
+    ref_ptr<StackedSphere> sphere = new StackedSphere(5, 200, 200);
     ref_ptr<Group> root = new Group();
     root->addChild(sphere.get());
     ref_ptr<Texture2D> texture = new Texture2D;
-    ref_ptr<Image> image = osgDB::readImageFile("../land_shallow_topo_2048.jpg");
+    ref_ptr<Image> image = osgDB::readImageFile("../Textures/EarthMap.jpg");
     texture->setImage(image);
-    // root->getOrCreateStateSet()->setTextureAttributeAndModes(0, texture.get());
+    texture->setWrap(Texture::WRAP_S,Texture::CLAMP_TO_EDGE);
+    root->getOrCreateStateSet()->setTextureAttributeAndModes(0, texture.get());
 
     // wenn man die Dreiecke mal sehen will:
     ref_ptr<PolygonMode> pm = new PolygonMode;
     pm->setMode(PolygonMode::FRONT_AND_BACK, PolygonMode::LINE);
-    root->getOrCreateStateSet()->setAttribute(pm.get());
+    //root->getOrCreateStateSet()->setAttribute(pm.get());
 
     osgViewer::Viewer viewer;
     viewer.setSceneData(root.get());
