30675e176b637e43fe3a3d11d0f16131ae6335f0
Benjamin <benjamin.held@stud.fh-hannover.de>
2013-05-26 11:59:37 +0200

    additional annitations for our sun

--- src/objects/Sun.cpp
+++ src/objects/Sun.cpp
@@ -7,26 +7,43 @@ ph::Sun::Sun(const double radius, const int lsteps, const int wsteps, const int 
 }
 
 void ph::Sun::createLight(int lightNumber) {
+
+	// Creating a white light source in the interior of the sphere
+	// creating a new light
     this->light = new Light;
+    // giving the light an OpenGL-Lightnumber [0,7]
     this->light->setLightNum(lightNumber);
+    // giving the light a position
+    // setPosition(x,y,z,sourcetype); sourcetype = {0.0 (global), 1.0 (local)}
     this->light->setPosition(Vec4(0.0,0.0,0.0,1.0));
+    // setting parameter for diffuse light
     this->light->setDiffuse(Vec4(1.0,1.0,1.0,1.0));
+    // setting parameter for specular light
     this->light->setSpecular(Vec4(1.0,1.0,1.0,1.0));
+    
+    // atm we dont want ambient light
     //this->light->setAmbient(Vec4(0.0,0.0,0.0,0.0));
     
+    // creating a new light source
     this->source = new LightSource;
+    // giving the source a light
     this->source->setLight(light);
+    // switch the light on
     this->source->setLocalStateSetModes(StateAttribute::ON);
     this->source->getOrCreateStateSet()->setMode(GL_LIGHTING, StateAttribute::ON);
     this->addChild(this->source);
     
+    // Creating Material for the Sun
     this->material = new Material;
+    // material emits giving light (R,G,B,x)
     this->material->setEmission(Material::FRONT_AND_BACK, Vec4(0.9,0.6,0.0,1.0));
     
+    // material parameters; perhaps we need some of them later
     //this->material->setDiffuse(Material::FRONT_AND_BACK, Vec4(1.0,0.0,0.0,1.0));
     //this->material->setAmbient(Material::FRONT_AND_BACK, Vec4(1.0,0.0,0.0,1.0));
     //this->material->setSpecular(Material::FRONT_AND_BACK, Vec4(1.0,0.0,0.0,1.0));
     //this->material->setShininess(Material::FRONT_AND_BACK, 1);
     
+    //giving the material to the sphere
     this->getOrCreateStateSet()->setAttributeAndModes(this->material.get(),StateAttribute::ON);
 }
--- src/objects/Sun.h
+++ src/objects/Sun.h
@@ -11,12 +11,12 @@ using namespace osg;
 namespace ph {
     class Sun : public Group {
         private:
-            ref_ptr<Light> light;
+            ref_ptr<Light> light; // light- and sourceelement of the sun
             ref_ptr<LightSource> source;
-            ref_ptr<Material> material;
+            ref_ptr<Material> material; // material of the sphere
             ref_ptr<Sphere> sphere;
             
-            void createLight(int lightNumber);
+            void createLight(int lightNumber); // lightNumber: OpenGLLightNumber [0,7]
         
         public:
             Sun(const double radius, const int lsteps, const int wsteps, const int lightNumber);
