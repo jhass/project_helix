1d05af3289b3cdf35c6737f04129e310e3becd9e
Benjamin <benjamin.held@stud.fh-hannover.de>
2013-06-11 13:59:00 +0200

    merge commit to be able to pull

--- src/runners/station.cpp
+++ src/runners/station.cpp
@@ -1,19 +1,27 @@
 #include <osgViewer/Viewer>
 #include <osg/Group>
-#include <osgDB/ReadFile>
 #include <osg/Node>
-#include <osg/StateSet>
-#include <osg/ShadeModel>
+#include <osg/MatrixTransform>
 
 #include "objects/Chronos.h"
+#include "objects/Ship.h"
 using namespace osg;
 
 int main(void) {
     ref_ptr<Group> root = new Group;
     ref_ptr<ph::Chronos> station = new ph::Chronos();
+    ref_ptr<ph::Ship> ship = new ph::Ship;
+    
+    osg::ref_ptr<osg::MatrixTransform> scale_station = new osg::MatrixTransform;
+    scale_station->setMatrix( Matrix::scale(4.0, 4.0, 4.0));
+    scale_station->addChild(station.get());
+    
+    osg::ref_ptr<osg::MatrixTransform> trans = new osg::MatrixTransform;
+    trans->setMatrix( Matrix::translate(-30.0, 0.0, 0.0));
+    trans->addChild(ship.get());
     
     root->addChild(station.get());
-    
+    //root->addChild( trans.get() );
  
     osgViewer::Viewer viewer;
     viewer.setSceneData(root.get());
