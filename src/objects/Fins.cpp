85212af65c393c5324f64621e9a62da2bf0417fa
Gnurfster <honk@wolke7.net>
2013-06-11 18:54:07 +0200

    Rotationskörper - Normalen Rotationskörper ergänzt - Flossen Normalen angepasst -> Irgendwas stimmt da noch nicht - ToDo: Antrieb -> Partikelsystem

--- src/objects/Fins.cpp
+++ src/objects/Fins.cpp
@@ -13,7 +13,8 @@ ph::Fins::Fins(const int position, const int factor) {
     this->position = position;
     this->factor = factor;
     this->fins = new Geometry;
-    this->addDrawable(this->fins.get());
+    
+    this->addDrawable(this->fins.get());                           
     this->compute();
 }
 
@@ -26,7 +27,7 @@ void ph::Fins::setTexture(const int textureNumber, const string filename) {
 
     ref_ptr<Texture2D> texture = new Texture2D;
     ref_ptr<Image> image = osgDB::readImageFile(filename);
-    texture->setWrap(Texture::WRAP_S, Texture::CLAMP_TO_EDGE);
+   // texture->setWrap(Texture::WRAP_S, Texture::CLAMP_TO_EDGE);
     texture->setImage(image.get());
     this->getOrCreateStateSet()->setTextureAttributeAndModes(textureNumber, texture.get());
 }
@@ -39,28 +40,44 @@ void ph::Fins::compute() {
 void ph::Fins::setFins(int factor) {
 	
     ref_ptr<Vec3Array> vertices = new Vec3Array();
-   
+     ref_ptr<Vec3Array> normals = new Vec3Array();
     ref_ptr<DrawElementsUInt> indices = new DrawElementsUInt(GL_TRIANGLE_STRIP);
     Vec3d coords;
-        ref_ptr<Vec2Array> texcoords = new Vec2Array;
+     ref_ptr<Vec2Array> texcoords = new Vec2Array;
         
     vertices->push_back(Vec3(0,0,0)); //Spitze (links)
+    normals->push_back(Vec3(0,0,1)); //Spitze (links)
+    
+    
     vertices->push_back(Vec3(1*factor,0,0)); //Heck
-    vertices->push_back(Vec3(1*factor,2*factor,0)); //Oben
-    vertices->push_back(Vec3(1*factor,0,2*factor)); // Vorne
-    vertices->push_back(Vec3(1*factor,0,-2*factor)); // Hinten
-    vertices->push_back(Vec3(1*factor,-2*factor,0)); //unten
+    normals->push_back(Vec3(1*factor,0,1)); //Heck
    
     
-    indices->push_back(0); indices->push_back(1); indices->push_back(2);
+    vertices->push_back(Vec3(1*factor,2*factor,0)); //Oben
+    normals->push_back(Vec3(1*factor,2*factor,1)); //Oben
+
+    
+    vertices->push_back(Vec3(1*factor,0,2*factor)); // Vorne
+    normals->push_back(Vec3(1*factor,1,2*factor)); // Vorne
+
+    
+    vertices->push_back(Vec3(1*factor,0,-2*factor)); // Hinten
+    normals->push_back(Vec3(1*factor,1,-2*factor)); // Hinten
+
+     
+    vertices->push_back(Vec3(1*factor,-2*factor,0)); //unten
+    normals->push_back(Vec3(1*factor,-2*factor,1)); //unten
+ 
+    
+    indices->push_back(2); indices->push_back(1); indices->push_back(0);
     indices->push_back(0); indices->push_back(1); indices->push_back(3);
-    indices->push_back(0); indices->push_back(1); indices->push_back(4);
+    indices->push_back(4); indices->push_back(1); indices->push_back(0);
     indices->push_back(0); indices->push_back(1); indices->push_back(5);
     
     this->fins->setVertexArray(vertices.get());
     this->fins->addPrimitiveSet(indices.get());
-    this->fins->setNormalBinding(Geometry::BIND_PER_VERTEX);
-    
+    this->fins->setNormalBinding(Geometry::BIND_OVERALL);  
+    //normalen noch spiegeln
 
 }
 
