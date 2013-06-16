6acc569288b49f6179cfc31fbaf6f8c785ac7d8e
chomper64 <richard.pump@gmx.de>
2013-06-16 16:28:31 +0200

    FIxing Ships path

--- src/animation/ShipNodeCallback.cpp
+++ src/animation/ShipNodeCallback.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-Vec3d ph::ShipNodeCallback::direction = Vec3d(0.05,0,0);
+Vec3d ph::ShipNodeCallback::direction = Vec3d(0.3,0,0);
 double ph::ShipNodeCallback::pitch = 0;
 double ph::ShipNodeCallback::yaw = 0;
 double ph::ShipNodeCallback::roll = 0;
@@ -19,7 +19,7 @@ void ph::ShipNodeCallback::operator()(Node* node, NodeVisitor* nv) {
 	ref_ptr<ph::Ship> shipNode = dynamic_cast<ph::Ship*>(node); //You don't want the long way.
 
 	//Rotate direction for translation
-	Quat q = Quat(pitch, Vec3d(1,0,0), roll, Vec3d(0,1,0), yaw, Vec3d(0,0,1));
+	Quat q = Quat(roll, Vec3d(1,0,0), pitch, Vec3d(0,1,0), yaw, Vec3d(0,0,1));
 	direction = q * direction;
 
 	//Rotate Ship
@@ -31,12 +31,13 @@ void ph::ShipNodeCallback::operator()(Node* node, NodeVisitor* nv) {
 	Matrix translation = Matrix::translate(shipNode->translate->getMatrix().getTrans() + direction);
 	shipNode->translate->setMatrix(translation);
 	traverse(node, nv);
+	pitch = yaw = roll = 0;
 }
 
 void ph::ShipNodeCallback::yawLeft() {
-	yaw = (yaw + PI/120);
+	yaw = (yaw + PI/30);
 }
 
 void ph::ShipNodeCallback::yawRight() {
-	yaw = (yaw - PI/120);
+	yaw = (yaw - PI/30);
 }
