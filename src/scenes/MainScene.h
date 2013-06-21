77c788d12d0488b8d3858337e0dcb325b8e54940
Jonne Haß <me@mrzyx.de>
2013-06-21 13:07:28 +0200

    move all creation methods into the main scene for now

--- src/scenes/MainScene.h
+++ src/scenes/MainScene.h
@@ -10,6 +10,16 @@ using namespace osg;
 
 namespace ph {
     class MainScene : public Group {
+        private:
+            void createSkyboxAndSuns();
+            void createPlanet();
+            void createStation();
+            void createShip();
+            void createAsteroidField();
+            void createComet();
+            void createCuboid();
+            void createReaper();
+            void createNebula();
         public:
             MainScene();
             ref_ptr<ph::Ship> ship;
